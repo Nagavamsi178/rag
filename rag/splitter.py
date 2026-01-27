649e457fa8965785f397eccbf1f0dc412bf53027
Nagavamsi178 <g.nagavamsi45@gmail.com>
2026-01-27 16:33:21 +0530

    Update splitter.py

--- rag/splitter.py
+++ rag/splitter.py
@@ -2,9 +2,9 @@ import streamlit as st
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 @st.cache_data(show_spinner=False)
-def split_documents(documents):
+def split_documents(_documents):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=150
     )
-    return splitter.split_documents(documents)
+    return splitter.split_documents(_documents)
